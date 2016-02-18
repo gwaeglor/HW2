15f707758604459b723fabc8f8385f2a1888ba06
Maryna Kolesnik <saga9119@gmail.com>
2016-02-19 00:41:53 +0200

    Homework3: Developers.

---
+++ developer.rb
@@ -0,0 +1,58 @@
+class Developer
+  MAX_TASKS = 10
+
+  def initialize(name)
+    @name = name
+    @task_list = []
+  end
+
+  def add_task(task)
+    begin
+    raise ArgumentError unless @task_list.size < max_tasks
+    rescue ArgumentError
+      'Слишком много работы!'
+    else
+      @task_list.push task
+      %Q{#{@name}: добавлена задача "#{task}". Всего в списке задач: #{@task_list.size}}
+    end
+  end
+
+  def tasks
+    @task_list.map.each_with_index { |v, i| "#{i + 1}. #{v}" }
+  end
+
+  def work!
+    begin
+      raise ArgumentError if @task_list.empty?
+    rescue ArgumentError
+      'Нечего делать!'
+    else
+      %Q{#{@name}: выполнена задача "#{@task_list.shift}". Осталось задач: #{@task_list.size}}
+    end
+  end
+
+  def can_add_task?
+    @task_list.size < max_tasks
+  end
+
+  def can_work?
+    !@task_list.empty?
+  end
+
+  def status
+    case
+      when !can_work?
+        'свободен'
+      when can_add_task? && can_work?
+        'работаю'
+      else
+        'занят'
+    end
+  end
+
+  private
+  def max_tasks
+    self.class::MAX_TASKS
+  end
+
+end
---
+++ senior_developer.rb
@@ -0,0 +1,14 @@
+require_relative 'developer'
+class SeniorDeveloper < Developer
+  MAX_TASKS = 15
+
+  def work!
+    begin
+      raise ArgumentError if @task_list.empty?
+    rescue ArgumentError
+      'Нечего делать!'
+    else
+      [0, 2].sample == 2 ? ([super, super]) : ('Что-то лень')
+    end
+  end
+end
