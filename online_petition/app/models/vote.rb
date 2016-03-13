a4775b09f76c025080f0832c290e6ec516985b0e
Maryna Kolesnik <saga9119@gmail.com>
2016-03-13 22:21:48 +0200

    Working on homework6: styles

--- online_petition/app/models/vote.rb
+++ online_petition/app/models/vote.rb
@@ -2,8 +2,6 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :petition
 
-  # validates_uniqueness_of :vote_id, scope: [:user_id, :petition_id]
-
   validates :user_id, presence: true
   validates :petition_id, presence: true
 end
